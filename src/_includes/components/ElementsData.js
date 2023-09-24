e6dbda40c70d6b67e85f4b5179e2c7ab7e533196
David Naylor <david.naylor.sf@gmail.com>
2023-09-24 20:15:23 +0000

    Extract using find

--- src/_includes/components/ElementsData.js
+++ src/_includes/components/ElementsData.js
@@ -1,5 +1,7 @@
 const elementsData = require("../../_data/ELEMENTS_v2.json");
 
+const record = elementsData.find((element) => element.Record.RecordNumber === 4);
+
 const first = elementsData[0];
 
 const second = elementsData[1];
