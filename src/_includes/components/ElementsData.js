6858b71e341894f23802a76871a5ba786038c803
David Naylor <david.naylor.sf@gmail.com>
2023-09-24 23:07:13 +0000

    extra details

--- src/_includes/components/ElementsData.js
+++ src/_includes/components/ElementsData.js
@@ -1,28 +1,23 @@
 const {html} = require('common-tags');
 const elementsData = require("../../_data/ELEMENTS_v2.json");
 
-const resp = ElementsData(3);
-console.log(resp);
-/* const record = elementsData.find((ITEM) => ITEM.Record.RecordNumber === 4);
-const props = record.Record.Section[1];
-const ids = record.Record.Section[0];
-const description = record.Record.Section[3];
-const response = {}; */
-
-/* response.properties = props;
-response.ids = ids;
-response.description = description; */
-
-
-
 function ElementsData(RecordNumber) {
   const record = elementsData.find((ITEM) => ITEM.Record.RecordNumber === RecordNumber);
+  identifiers = record.Record.Section[0];
   props = record.Record.Section[1];
-  propName = props.Section[0].Information[0].Name;
+  seca=0;
+  secb=2;
+  secc=3;
+
   return html`
     <div>
-      <h3>${props.Section[0].Information[0].Name}: ${props.Section[0].Information[0].Value.StringWithMarkup[0].String}</h3>
-      <h4>Source Ref: ${props.Section[0].Information[0].ReferenceNumber}</h4>
+      <h3>${identifiers.Section[0].Information[0].Name}: ${identifiers.Section[0].Information[0].Value.StringWithMarkup[0].String}</h3>    
+      <p>${props.Section[seca].Information[0].Name}: ${props.Section[seca].Information[0].Value.StringWithMarkup[0].String}</p>
+      <p>Source Ref: ${props.Section[seca].Information[0].ReferenceNumber}</p>
+      <p>${props.Section[secb].TOCHeading}: ${props.Section[secb].Information[0].Value.StringWithMarkup[0].String}</p>
+      <p>Source Ref: ${props.Section[secb].Information[0].ReferenceNumber}</p>
+      <p>${props.Section[secc].TOCHeading}: ${props.Section[secc].Information[0].Value.StringWithMarkup[0].String}</p>
+      <p>Source Ref: ${props.Section[secc].Information[0].ReferenceNumber}</p>
     </div>
   `;
 }
