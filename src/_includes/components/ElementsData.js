620e81076e51d183a35192e4620447113ce6e087
David Naylor <david.naylor.sf@gmail.com>
2023-09-24 22:47:50 +0000

    5 cards

--- src/_includes/components/ElementsData.js
+++ src/_includes/components/ElementsData.js
@@ -1,19 +1,24 @@
 const {html} = require('common-tags');
 const elementsData = require("../../_data/ELEMENTS_v2.json");
 
-const record = elementsData.find((ITEM) => ITEM.Record.RecordNumber === 4);
+const resp = ElementsData(3);
+console.log(resp);
+/* const record = elementsData.find((ITEM) => ITEM.Record.RecordNumber === 4);
 const props = record.Record.Section[1];
 const ids = record.Record.Section[0];
 const description = record.Record.Section[3];
-const response = {};
+const response = {}; */
 
-response.properties = props;
+/* response.properties = props;
 response.ids = ids;
-response.description = description;
+response.description = description; */
 
-const displayed = props.Section[0].Information[0].Name
 
-function ElementsData() {
+
+function ElementsData(RecordNumber) {
+  const record = elementsData.find((ITEM) => ITEM.Record.RecordNumber === RecordNumber);
+  props = record.Record.Section[1];
+  propName = props.Section[0].Information[0].Name;
   return html`
     <div>
       <h3>${props.Section[0].Information[0].Name}: ${props.Section[0].Information[0].Value.StringWithMarkup[0].String}</h3>
