03bb5d3e8de4d124c766e10a88bbd2f5003ee45d
David Naylor <david.naylor.sf@gmail.com>
2023-10-04 15:15:43 +0000

    New data files

--- src/_includes/components/ElementsData.js
+++ src/_includes/components/ElementsData.js
@@ -19,8 +19,8 @@ function ElementsData(RecordNumber) {
     <div>
       <h3>${identifiers.Section[0].Information[0].Name}: ${identifiers.Section[0].Information[0].Value.StringWithMarkup[0].String}</h3>
       <p>${props.Section[seca].Information[0].Name}: ${props.Section[seca].Information[0].Value.StringWithMarkup[0].String}, [ref:${props.Section[seca].Information[0].ReferenceNumber}]</p>
-      <p>${props.Section[secb].TOCHeading}: ${props.Section[secb].Information[0].Value.StringWithMarkup[0].String} [ref:${props.Section[secb].Information[0].ReferenceNumber}]</p>
-      <p>${props.Section[secc].TOCHeading}: ${props.Section[secc].Information[0].Value.StringWithMarkup[0].String} [ref:${props.Section[secc].Information[0].ReferenceNumber}]</p>
+      <p>${props.Section[secb].TOCHeading}: ${props.Section[secb].Information[0].Value.StringWithMarkup[0].String}, [ref:${props.Section[secb].Information[0].ReferenceNumber}]</p>
+      <p>${props.Section[secc].TOCHeading}: ${props.Section[secc].Information[0].Value.StringWithMarkup[0].String}, [ref:${props.Section[secc].Information[0].ReferenceNumber}]</p>
       
       
       
