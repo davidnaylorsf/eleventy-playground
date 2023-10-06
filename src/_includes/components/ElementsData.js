13b02a08d1ec0c43d3a3292e1fa19df652c6c9d0
David Naylor <david.naylor.sf@gmail.com>
2023-10-06 21:39:35 +0000

    Started info extraction from new data

--- src/_includes/components/ElementsData.js
+++ src/_includes/components/ElementsData.js
@@ -1,9 +1,9 @@
 const {html} = require('common-tags');
 // const elementsData = require("../../_data/ELEMENTS_v2.json");
 const references_data = require("../../_data/References_v1.json");
-const sections_data = require("../../_data/Sections_v1.json");
-const subInformations_data = require("../../_data/SubInformations_v1.json");
-const subSections_data = require("../../_data/SubSections_v1.json");
+const sections_data = require("../../_data/Sections_v2.json");
+//const subInformations_data = require("../../_data/SubInformations_v1.json");
+const subSections_data = require("../../_data/SubSections_v2.json");
 
 
 function RecordTitle(RecordNumber) {
@@ -25,26 +25,23 @@ function Section(RecordNumber, SectionName) {
     (ITEM) => ITEM.Section.TOCHeading === SectionName)[0];
   const subSections = subSections_data.filter(
     (ITEM) => ITEM.RecordNumber === RecordNumber && ITEM.SectionName === SectionName);
-  const subInformations = subInformations_data.filter(
-    (ITEM) => ITEM.RecordNumber === RecordNumber && ITEM.SectionName === SectionName);
-  //const subInformation = subInformations.filter(
-  //  (ITEM) => ITEM.Section.TOCHeading === SectionName)[0]
-  subInfo0 = subInformations[0].HTMLstring  
 
   infoStrings = [];
-  for ( i in subInformations ) {
-    if ( 'HTMLstring' in subInformations[i] ) {
-      infoStrings.push(subInformations[i].HTMLstring);
-    } else {
-      infoStrings.push(subInformations[i].string);
+  const SectionInfo = section.Info;
+    for (info of SectionInfo) {
+      if (info.htmlString.length > 0) {
+        infoStrings.push(info.htmlString);
+      }
+      
     }
+  
+    testSection1 = section[0];
+    testSection2 = section[1];
+
+
     
-  }
 
-  // console.log(infoStrings);
 
-  // sectionInfo1 =  section.Section.Information[0].Value.StringWithMarkup[0].String
- 
 
 
   return html`
